0027d442033d41c8824676c5ecef5b0b53884102
BrunoCastroTomaz <brunoworkjobs@gmail.com>
2024-08-29 19:09:01 -0300

    Add first styles

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,10 +1,14 @@
 // JSX = JavaScript + XML
 
+import { Header } from './components/Header';
 import { Post } from './Post';
+
+import './styles.css';
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post 
         author="Bruno Castro"
         content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
