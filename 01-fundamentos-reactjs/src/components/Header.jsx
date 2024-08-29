90afdddc76d6990d6ab217978f170e36cd9a2ddc
BrunoCastroTomaz <brunoworkjobs@gmail.com>
2024-08-29 19:57:07 -0300

    add header component

--- 01-fundamentos-reactjs/src/components/Header.jsx
+++ 01-fundamentos-reactjs/src/components/Header.jsx
@@ -1,8 +1,10 @@
 import styles from './header.module.css'
+import igniteLogo from '../assets/ignite-logo.svg'
 
 export function Header(){
 	return (
 		<header className={styles.header}>
+			<img src={igniteLogo} alt = "Logotipo Social Bruno" />
 			<strong>Social Bruno</strong>
 		</header>
 	);
